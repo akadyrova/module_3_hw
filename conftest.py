0cea09af1d2cb92a95a4f3194d547c630b628e67
akadyrova <akadyrova@yandex.ru>
2021-09-21 16:23:14 +0300

    options added

--- conftest.py
+++ conftest.py
@@ -1,18 +1,23 @@
 import pytest
 from selenium import webdriver
 import time
+from selenium.webdriver.chrome.options import Options
+
+
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None)
 
 @pytest.fixture(scope="function")
 def browser(request):
+    options = Options()
     language = request.config.getoption("language")
-    browser=webdriver.Chrome()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser=webdriver.Chrome(options=options)
     browser.implicitly_wait(10)
-    link="http://selenium1py.pythonanywhere.com/"+language+"/catalogue/coders-at-work_207/"
+    link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(30)
+    time.sleep(5)
     yield browser
     print("\nquit browser..")
     browser.quit()
